f83faa289ac43e45eb095cb4dc68eec26826e628
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-12-14 18:58:30 -0200

    Behaviors should implement CompletableTransformer

--- core-architecture/src/main/java/br/ufs/architecture/core/presentation/behaviors/BehaviorsPresenter.kt
+++ core-architecture/src/main/java/br/ufs/architecture/core/presentation/behaviors/BehaviorsPresenter.kt
@@ -3,10 +3,7 @@ package br.ufs.architecture.core.presentation.behaviors
 import br.ufs.architecture.core.presentation.errorstate.AssignErrorState
 import br.ufs.architecture.core.presentation.loading.LoadingCoreographer
 import br.ufs.architecture.core.presentation.networking.NetworkingErrorFeedback
-import io.reactivex.Observable
-import io.reactivex.ObservableSource
-import io.reactivex.ObservableTransformer
-import io.reactivex.Scheduler
+import io.reactivex.*
 
 /**
  *
@@ -16,14 +13,21 @@ import io.reactivex.Scheduler
 
 class BehaviorsPresenter(
         private val view: Any,
-        private val scheduler: Scheduler) : ObservableTransformer<Any, Any> {
+        private val scheduler: Scheduler) :
+        ObservableTransformer<Any, Any>, CompletableTransformer {
+
+    override fun apply(upstream: Completable): CompletableSource {
+        return pipeline(upstream.toObservable<Any>()).ignoreElements()
+    }
 
     override fun apply(upstream: Observable<Any>): ObservableSource<Any> {
+        return pipeline(upstream)
+    }
 
+    private fun pipeline(upstream: Observable<Any>): Observable<Any> {
         return upstream
                 .compose(AssignErrorState(view, scheduler))
                 .compose(NetworkingErrorFeedback(view, scheduler))
                 .compose(LoadingCoreographer(view, scheduler))
     }
-
 }
