f97140367f231d15adff7b67a212d6ec6be49afc
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-12-14 01:41:40 -0200

    Using Onboarding for first screen related Android files

--- app/src/main/java/br/ufs/hiring/stone/OnboardingActivity.kt
+++ app/src/main/java/br/ufs/hiring/stone/OnboardingActivity.kt
@@ -3,10 +3,10 @@ package br.ufs.hiring.stone
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding)
     }
 }
