dd4ad92b9b202bc98b5acd1cd3f50e00fd41c8d8
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-12-26 23:21:52 -0200

    Refactoring to use fixture data

--- features/src/test/java/br/ufs/hiring/stone/features/tests/wallet/ParseTuplesTests.kt
+++ features/src/test/java/br/ufs/hiring/stone/features/tests/wallet/ParseTuplesTests.kt
@@ -1,15 +1,13 @@
 package br.ufs.hiring.stone.features.tests.wallet
 
 import br.ufs.architecture.core.errors.InfrastructureError.LocalDatabaseAccessError
-import br.ufs.hiring.stone.data.database.BrokingRow
-import br.ufs.hiring.stone.data.database.SavingRow
 import br.ufs.hiring.stone.data.database.TransactionRow
 import br.ufs.hiring.stone.domain.TransactionType
+import br.ufs.hiring.stone.features.tests.fixtures.Fixtures
 import br.ufs.hiring.stone.features.wallet.ParseTuples
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
-import java.util.*
 
 /**
  *
@@ -18,45 +16,10 @@ import java.util.*
  */
 class ParseTuplesTests {
 
-    private val owner = UUID.randomUUID().toString()
-
-    private val brokingRows = listOf(
-            BrokingRow(
-                    updatedAt = 123456,
-                    label = "bta",
-                    buyPrice = 3.14f,
-                    sellPrice = 3.13f
-            )
-    )
-
-    private val savingRows = listOf(
-            SavingRow(
-                    ownerId = owner,
-                    label = "blr",
-                    amount = 50000f
-            ),
-            SavingRow(
-                    ownerId = owner,
-                    label = "btc",
-                    amount = 1f
-            ),
-            SavingRow(
-                    ownerId = owner,
-                    label = "bta",
-                    amount = 1000f
-            )
-    )
-
-    private val transactionsRows = listOf(
-            TransactionRow(
-                    ownerId = owner,
-                    amount = 1.0f,
-                    type = "buy",
-                    timestamp = "2017-12-13T03:12:31.821",
-                    currency = "btc"
-            )
-    )
-
+    private val owner = Fixtures.owner
+    private val brokingRows = Fixtures.brokingRows
+    private val savingRows = Fixtures.savingRows
+    private val transactionsRows = Fixtures.transactionsRows
 
     @Test fun `should return parsed information from tuples`() {
 
